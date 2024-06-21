74defb63f11919b30c82028babe8a6ef37955ec7
Cassian Godsted <cassian@godsted.com>
2024-06-21 17:13:12 -0400

    Tooltip now only displays when Control is held

--- src/main/java/cc/cassian/lore/client/TooltipClient.java
+++ src/main/java/cc/cassian/lore/client/TooltipClient.java
@@ -2,6 +2,7 @@ package cc.cassian.lore.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -11,7 +12,9 @@ public class TooltipClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ItemTooltipCallback.EVENT.register((stack, context, type, lines) -> {
-            lines.add(Text.translatable(findLoreKey(stack)).formatted(Formatting.GRAY));
+            if (Screen.hasControlDown()) {
+                lines.add(Text.translatable(findLoreKey(stack)).formatted(Formatting.GRAY));
+            }
         });
     }
 }
