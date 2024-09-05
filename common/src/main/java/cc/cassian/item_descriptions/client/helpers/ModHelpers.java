b6dd975ac7e801c2ed7b33d26f90f72c7d5d028d
Cassian Godsted <cassian@godsted.com>
2024-09-05 13:23:21 -0400

    Disable Limelight text wrapping if width is set to 0

--- common/src/main/java/cc/cassian/item_descriptions/client/helpers/ModHelpers.java
+++ common/src/main/java/cc/cassian/item_descriptions/client/helpers/ModHelpers.java
@@ -359,7 +359,7 @@ public class ModHelpers {
     }
 
     public static Text createMultilineTranslation(String loreKey) {
-        //Setup list to store (potentially multi-line) tooltip.
+//        Setup list to store (potentially multi-line) tooltip.
         StringBuilder lines = new StringBuilder();
         int maxLength = ModConfig.get().compat_limelightLength;
         //Check if the key exists.
@@ -368,15 +368,18 @@ public class ModHelpers {
             String translatedKey = translate(loreKey);
             //Check if the translated key exists.
             if (hasTranslation(loreKey)) {
-                //Any tooltip longer than 25 characters should be shortened.
-                while (translatedKey.length() >= maxLength) {
-                    //Find how much to shorten the tooltip by.
-                    int index = getIndex(translatedKey, maxLength);
-                    //Add a shortened tooltip.
-                    lines.append(translatedKey, 0, index);
-                    lines.append("\n");
-                    //Remove the shortened tooltip substring from the tooltip. Repeat.
-                    translatedKey = translatedKey.substring(index);
+                //Disable text wrapping if max length is set to 0.
+                if (maxLength != 0) {
+                    //Any tooltip longer than XX characters should be shortened.
+                    while (translatedKey.length() >= maxLength) {
+                        //Find how much to shorten the tooltip by.
+                        int index = getIndex(translatedKey, maxLength);
+                        //Add a shortened tooltip.
+                        lines.append(translatedKey, 0, index);
+                        lines.append("\n");
+                        //Remove the shortened tooltip substring from the tooltip. Repeat.
+                        translatedKey = translatedKey.substring(index);
+                    }
                 }
                 //Add the final tooltip.
                 lines.append(translatedKey);
