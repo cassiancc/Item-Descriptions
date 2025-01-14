d694676458f23813bd948552af9cb1bb7c276980
Cassian Godsted <cassian@godsted.com>
2025-01-13 23:19:36 -0500

    remove unnecessary stonecutter

--- src/main/java/cc/cassian/item_descriptions/client/mixin/InfoOverlayMixin.java
+++ src/main/java/cc/cassian/item_descriptions/client/mixin/InfoOverlayMixin.java
@@ -22,7 +22,6 @@ import static cc.cassian.item_descriptions.client.helpers.ModHelpers.*;
 @Pseudo
 @Mixin(InfoOverlay.class)
 public class InfoOverlayMixin {
-    //? if >1.20 {
     @Inject(method = "setHitResult", at = @At(value = "INVOKE", target = "Lcom/brokenkeyboard/usefulspyglass/InfoOverlay;setComponent(Ljava/util/List;)V", ordinal = 0), remap = false)
     private static void spyglassEntityDescriptions(HitResult result, CallbackInfo ci, @Local ArrayList<TooltipInfo> tooltipList, @Local LivingEntity entity) {
         if (showEntityDescriptions()) {
@@ -44,5 +43,4 @@ public class InfoOverlayMixin {
             }
         }
     }
-    ///?}
 }
