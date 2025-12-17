c5e9bb0684ae843ca1715418c726a0a65ed50af8
Cassian Godsted <cassian@godsted.com>
2025-12-16 19:19:51 -0500

    remove use of redirect

--- src/main/java/cc/cassian/item_descriptions/client/mixin/StatusEffectsDisplayMixin.java
+++ src/main/java/cc/cassian/item_descriptions/client/mixin/StatusEffectsDisplayMixin.java
@@ -21,7 +21,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Collection;
@@ -63,8 +62,8 @@ public abstract class StatusEffectsDisplayMixin {
     *///?} else if >1.21.5 {
     @Shadow protected abstract void renderLabels(GuiGraphics context, int x, int height, Iterable<MobEffectInstance> statusEffects);
 
-    @Redirect(method = "renderTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;setTooltipForNextFrame(Lnet/minecraft/client/gui/Font;Ljava/util/List;Ljava/util/Optional;II)V"))
-    private void renderEffects(GuiGraphics context, Font textRenderer, List<Component> text, Optional<TooltipComponent> data, int mouseX, int mouseY) {
+    @WrapOperation(method = "renderTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;setTooltipForNextFrame(Lnet/minecraft/client/gui/Font;Ljava/util/List;Ljava/util/Optional;II)V"))
+    private void renderEffects(GuiGraphics context, Font textRenderer, List<Component> text, Optional<TooltipComponent> data, int mouseX, int mouseY, Operation<Void> original) {
         List<Component> tooltipText = EffectDescriptions.createEffectDescription(text);
         context.setComponentTooltipForNextFrame(textRenderer, tooltipText, mouseX, mouseY);
     }
@@ -80,13 +79,7 @@ public abstract class StatusEffectsDisplayMixin {
         booleanRef.set(false);
     }
 
-    //?} else if >1.21.1 {
-    /*@Redirect(method = "renderEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;renderTooltip(Lnet/minecraft/client/gui/Font;Ljava/util/List;Ljava/util/Optional;II)V"))
-    private void renderEffects(GuiGraphics context, Font textRenderer, List<Component> text, Optional<TooltipComponent> data, int mouseX, int mouseY) {
-        List<Component> tooltipText = EffectDescriptions.createEffectDescription(text);
-        context.renderTooltip(textRenderer, tooltipText, Optional.empty(), mouseX, mouseY);
-    }
-    *///?} else {
+    //?} else {
     /*@WrapOperation(method = "renderEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;renderTooltip(Lnet/minecraft/client/gui/Font;Ljava/util/List;Ljava/util/Optional;II)V"))
     private void renderEffects(GuiGraphics context, Font textRenderer, List<Component> text, Optional<TooltipComponent> visualTooltipComponent, int mouseX, int mouseY, Operation<Void> original) {
         List<Component> tooltipText = EffectDescriptions.createEffectDescription(text);
